5f9ac8af8cb9fb13a81ef138152d9882c149314c
Powered-designs <brendanpower@protonmail.com>
2021-03-10 12:41:37 +0200

    update

---
+++ precache-manifest.9870e20d4bf704f5475581127d389e7c.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "41c01fd4f492be47e2c571553bf4d66b",
+    "url": "./index.html"
+  },
+  {
+    "revision": "d98cfbb43866ed995bf3",
+    "url": "./static/css/2.87a9c7a2.chunk.css"
+  },
+  {
+    "revision": "1d7aeb133c2ee41def91",
+    "url": "./static/css/main.eabb8938.chunk.css"
+  },
+  {
+    "revision": "d98cfbb43866ed995bf3",
+    "url": "./static/js/2.e6af11ed.chunk.js"
+  },
+  {
+    "revision": "e6b8f1912a8edbcb3fe64525dc85e2b4",
+    "url": "./static/js/2.e6af11ed.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1d7aeb133c2ee41def91",
+    "url": "./static/js/main.e89a2fab.chunk.js"
+  },
+  {
+    "revision": "9b8a447012d2433ab7ea",
+    "url": "./static/js/runtime-main.9bc9a3aa.js"
+  },
+  {
+    "revision": "4fbd5fd14f8f37cdf3dd7f1fdbdbebe9",
+    "url": "./static/media/about.4fbd5fd1.mp4"
+  },
+  {
+    "revision": "c1a6e5cb14188f5fe6283757b4c20d94",
+    "url": "./static/media/logo.c1a6e5cb.png"
+  }
+]);
